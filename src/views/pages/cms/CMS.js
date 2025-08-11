be94c928ecafd4f422272a05fcca3c669de94e1d
jhangikhel <gourav.jhangikhel@gmail.com>
2025-08-11 16:05:46 +0530

    Add video

--- src/views/pages/cms/CMS.js
+++ src/views/pages/cms/CMS.js
@@ -1,36 +1,60 @@
 import React, { useEffect, useState } from 'react'
-import { CFormSelect, CCol, CRow } from '@coreui/react'
+import { CFormSelect, CCol, CRow, CButton } from '@coreui/react'
 import axios from 'axios'
 
 import Editor from 'react-simple-wysiwyg'
+import { data } from 'react-router-dom'
 
 const CMS = () => {
-  const [html, setHtml] = useState('')
-  const [selectedCms, setSelectedCMS] = useState(1)
-  const onChange = (e) => {
-    setHtml(e.target.value)
-  }
+    const [html, setHtml] = useState('')
+    const [selectedCms, setSelectedCMS] = useState(1)
+    const onChange = (e) => {
+        setHtml(e.target.value)
+    }
+    useEffect(() => {
+        axios.get(`https://payebuddy.xyz/api/cms/${selectedCms}`).then((r) => {
+            setHtml(r.data.text)
+        })
+    }, [selectedCms])
+    const saveData = () => {
+        alert(html)
+        if (!html) {
+            alert("Please enter text");
+            return;
+        }
+        axios.put(`https://payebuddy.xyz/api/cms/${selectedCms}`, {
 
-  return (
-    <>
-      <CRow>
-        <CCol className="mb-3 mb-sm-0" sm={6} lg={5}>
-          <CFormSelect
-            value={selectedCms}
-            size="lg"
-            className="mb-3"
-            onChange={(e) => {
-              setSelectedCMS(e.target.value)
-            }}
-            aria-label="Large select example"
-          >
-            <option value="1">FAQs</option>
-            <option value="2">Terms and Condition</option>
-          </CFormSelect>
-        </CCol>
-      </CRow>
-      <Editor value={html} onChange={onChange} style={{height:"300px"}} />
-    </>
-  )
+            text: html,
+
+        })
+    }
+    return (
+        <>
+            <CRow>
+                <CCol className="mb-3 mb-sm-0" sm={6} lg={5}>
+                    <CFormSelect
+                        value={selectedCms}
+                        size="lg"
+                        className="mb-3"
+                        onChange={(e) => {
+                            setSelectedCMS(e.target.value)
+                        }}
+                        aria-label="Large select example"
+                    >
+                        <option value="1">FAQs</option>
+                        <option value="2">Terms and Condition</option>
+                    </CFormSelect>
+                </CCol>
+            </CRow>
+            <Editor value={html} onChange={onChange} style={{ height: '300px' }} />
+            <CRow className="mt-3 justify-content-center">
+                <CCol xs={4}>
+                    <CButton color="primary" onClick={saveData} size="md">
+                        Submit
+                    </CButton>
+                </CCol>
+            </CRow>
+        </>
+    )
 }
 export default CMS
